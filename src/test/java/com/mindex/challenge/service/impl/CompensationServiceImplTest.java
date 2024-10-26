b8888a1c5efe5bdb6ffdd038ddb348ef61df23a5
mindex <dev@mindex.com>
2024-10-26 16:06:55 -0400

    Created CompensationServiceImplTest to test the Compensation Service.

---
+++ src/test/java/com/mindex/challenge/service/impl/CompensationServiceImplTest.java
@@ -0,0 +1,65 @@
+package com.mindex.challenge.service.impl;
+
+import com.mindex.challenge.data.Compensation;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CompensationServiceImplTest {
+
+    private String compensationUrl;
+    private String compensationIdUrl;
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Before
+    public void setup() {
+        compensationUrl = "http://localhost:" + port + "/compensation";
+        compensationIdUrl = "http://localhost:" + port + "/compensation/{id}";
+    }
+
+    @Test
+    public void testCreateRead() throws ParseException {
+        Compensation testCompensation = new Compensation();
+        testCompensation.setEmployeeId("16a596ae-edd3-4847-99fe-c4518e82c86f");
+        testCompensation.setSalary(100000);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        testCompensation.setEffectiveDate(sdf.parse("2021-01-01"));
+
+        // Create checks
+        Compensation createdCompensation = restTemplate.postForEntity(compensationUrl, testCompensation, Compensation.class).getBody();
+
+        assert createdCompensation != null;
+        assertNotNull(createdCompensation.getEmployeeId());
+        assertCompensationEquivalence(testCompensation, createdCompensation);
+
+        // Read checks
+        Compensation readCompensation = restTemplate.getForEntity(compensationIdUrl, Compensation.class, createdCompensation.getEmployeeId()).getBody();
+        assertEquals(createdCompensation.getEmployeeId(), readCompensation.getEmployeeId());
+    }
+
+    private static void assertCompensationEquivalence(Compensation expected, Compensation actual) {
+        assertEquals(expected.getEmployeeId(), actual.getEmployeeId());
+        assertEquals(expected.getSalary(), actual.getSalary(), 0.0001);
+        assertEquals(expected.getEffectiveDate(), actual.getEffectiveDate());
+    }
+
+}
